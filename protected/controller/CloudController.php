d46f7156cb8673a13b745a8cac7d2b35bd6a56b0
David Diao <daviddiaoyf@gmail.com>
2019-01-25 19:30:47 +0800

    Require login to check file

--- protected/controller/CloudController.php
+++ protected/controller/CloudController.php
@@ -8,6 +8,7 @@ class CloudController extends BaseController
 
     public function actionFileExists()
     {
+        if (!$this->is_login) ERR::Catcher(2001);
         if (!arg('hash')) ERR::Catcher(1003);
         $hash = strtolower(arg('hash'));
         if (!preg_match('/^[0-9a-f]{40}$/', $hash)) ERR::Catcher(1004);
