9b0bf73a817a6d853efe1d45906198151b6ddd94
David Diao <daviddiaoyf@gmail.com>
2019-01-25 19:24:20 +0800

    Add file existance check function

--- protected/controller/CloudController.php
+++ protected/controller/CloudController.php
@@ -5,4 +5,12 @@ class CloudController extends BaseController
     {
         $this->title='SAST Cloud';
     }
+
+    public function actionFileExists()
+    {
+        if (!arg('hash')) ERR::Catcher(1003);
+        $hash = strtolower(arg('hash'));
+        if (!preg_match('/^[0-9a-f]{40}$/', $hash)) ERR::Catcher(1004);
+        SUCCESS::Catcher('success', file_exists(CONFIG::GET('CLOUD_FILE_DIRECTORY').DS.substr($hash, 0, 2).DS.substr($hash, 2)));
+    }
 }
---
+++ protected/model/ERR.php
@@ -0,0 +1,44 @@
+<?php
+
+class ERR {
+    
+    /**
+     * An old-fashioned error catcher mainly to provide error description
+     * existed here only to avoid direct SQL database access
+     * return a hundred present pure string
+     *
+     * @author John Zhang
+     * @param string $ERR_CODE
+     */
+
+    public static function Catcher($ERR_CODE)
+    {
+        if(($ERR_CODE<1000)) $ERR_CODE=1000;
+        $output=array(
+             'ret' => $ERR_CODE,
+            'desc' => self::Desc($ERR_CODE),
+            'data' => null
+        );
+        exit(json_encode($output));
+    }
+     
+    private static function Desc($ERR_CODE)
+    {
+        $ERR_DESC=array(
+            
+            '1000' => "Unspecified Error",  /** Under normal condictions those errors shouldn't displayed to end users unless they attempt to do so
+                                             *  some submissions should be intercepted by the frontend before the request sended 
+                                             */
+            '1001' => "Internal Sever Error : SECURE_VALUE 非法",
+            '1002' => "内部服务器错误：操作失败",
+            '1003' => "内部服务器错误：参数不全",
+            '1004' => "内部服务器错误：参数非法",
+            '1005' => "内部服务器错误：文件类型不被支持",
+            '1006' => "内部服务器错误：输入过长",
+            
+            '6000' => 'Cloud-Related Error',
+        );
+        return isset($ERR_DESC[$ERR_CODE])?$ERR_DESC[$ERR_CODE]:$ERR_DESC['1000'];
+    }
+
+}
