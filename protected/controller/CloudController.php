8047dcfcfc88cbf84ec38e2366cc922c6c74362e
Tasumi <18851980988@163.com>
2019-02-10 00:49:04 +0800

    add show function

--- protected/controller/CloudController.php
+++ protected/controller/CloudController.php
@@ -22,4 +22,77 @@ class CloudController extends BaseController
         $result = $users->find(['uid=:uid', ':uid'=>$_SESSION['uid']]);
         SUCCESS::Catcher('success', ['total'=>intval($result['capacity']), 'used'=>intval($result['used']), 'available'=>$result['capacity']-$result['used']]);
     }
+
+    //显示文件及文件夹    
+    public function actionShow()
+    {
+        if (!$this->islogin) ERR::Catcher(2001);
+        if (!arg('path')) ERR::Catcher(1003);
+        if(!is_path_legal(arg('path'))) ERR::Catcher(1004);
+        if(!is_path_existed(arg('path'))) ERR::Catcher(6002);
+        $name=getName(arg('path'));
+        $path=$name[1];
+        $db=new Model("disk_file");
+        SUCCESS::Catcher('success',$db->query('select filename,time,filesize,is_dir from disk_file where uid=:uid,path=:path,deleted=0',':uid'=>$_SESSION['uid'],':path'=>$path));
+    }
+
+
+    
+
+    //用到的函数
+
+    public function getSHA($path)
+    {
+        $fp = fopen($path, 'rb');
+        $ctx = hash_init('sha1');
+        hash_update($ctx, filesize($path)."\0");
+        while (!feof($fp)) {
+            hash_update($ctx, fread($fp, 65536));
+        }
+        return hash_final($ctx);
+    }
+    
+    //文件夹是否存在
+    public function is_path_legal($path)
+    {
+        $pattern='/[\:*?"<>|]|\/\.\/|\/\.\.\//';
+        if(!preg_match($pattern, $path)) return false;
+        return true;
+    }
+
+    public function is_path_existed($path)
+    {
+        $db=new Model("disk_file");
+        $condition=array('uid'=>$_SESSION['uid'],'path'=>$path);
+        $result=$db->findAll($condition);
+        if(empty($result)) return false;
+        return true;
+
+    }
+
+    public function getName($path)
+    {
+        //是否以/结尾
+        if(substr_compare($path, '/', -strlen('/')) === 0)
+        {
+            $name=array();
+            if(strcmp($path,'/')) $name[1]='/' return $name;
+            $path=rtrim($arr_str,'/');
+            $result=explode('/',$path);
+            $name[0]=$result[count($result)-1];
+            $name[1]=substr($path,0,strlen($path)-strlen($name[0])-1);
+            return $name;
+        }
+        else
+        {
+
+            $name=array();
+            $result=explode('/',$path);
+            $name[0]=$name[0]=$result[count($result)-1];;
+            $name[1]=substr($path,0,strlen($path)-strlen($name[0])-1);
+            return $name;
+        }
+    }
+
+
 }
